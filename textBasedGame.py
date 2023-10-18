3f255e034aca6cc71cecb8ebdf68ca4a9ee5ba66
John St Hilaire <JStHilaire@pannosmarketing.com>
2023-10-18 15:25:27 -0400

    fixed game loop to move through room objects

--- textBasedGame.py
+++ textBasedGame.py
@@ -68,37 +68,58 @@ def clear_screen():
 class Room:
     def __init__(self, name):
         self.name = name
-        self.rooms = roomsDirections[self.name]
+        self.directions = roomsDirections[self.name]
         self.description = roomDescriptions[self.name]
         self.items = roomItems[self.name]
 
     def enter(self):
         print(self.description)
         # additional logic here
+    
+    # prints description of room
+    def examine(self):
+        print(self.discription)
+
+    # search room for items and user choice to pick up item
+    def search(self):
+        print(f"You search the room and find:")
+        for i, item in enumerate(self.items):
+            print(f"{i+1}. {item}")
+            
+        itemSelection = int(input("Enter the number of the item you would like to pick up"))
+        if itemSelection - 1 <= len(self.items):
+            inventory.append(self.items[itemSelection - 1])
+            self.items.pop(itemSelection - 1)
+        else:
+            print("Enter a number listed")
+        
 
 # room object declaration
 rooms = ["Plane Wreck", "Crash Site", "Jungle Path", "Top of Temple Stairs", "Temple Entrance", "Creepy Library", "Tomb", "Treasure Room", "Demon's Lair"]
+
+# dictionary with all room objects
 roomObjMap = {x : Room(x) for x in rooms}
 
-# init starting room and game state
+# init starting room game state, and inventory
 playing = True
-currentRoom = 'Plane Wreck'
+currentRoom = roomObjMap['Plane Wreck']
+inventory = []
 
 # game loop
 while playing:
     # Display current room
-    print(f"You are in {currentRoom}.")
+    print(f"You are in {currentRoom.name}.")
 
     # Display available directions
-    for direction, room in rooms[currentRoom].items():
+    for direction, room in currentRoom.directions.items():
         print(f"Direction: {direction}, Room: {room}")
 
     # Get player input
     newDirection = input("Which direction would you like to go? Type 'exit' to exit the game.\n").capitalize()
 
     # Check if direction is valid
-    if newDirection in rooms[currentRoom]:
-        currentRoom = rooms[currentRoom][newDirection]
+    if newDirection in currentRoom.directions:
+        currentRoom = roomObjMap[currentRoom.directions[newDirection]]
     elif newDirection == 'Exit':
         playing = False
     else:
