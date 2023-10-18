92c2b55442f982117d6c5b74fc8861fdd8f2a25f
John St Hilaire <JStHilaire@pannosmarketing.com>
2023-10-18 12:11:51 -0400

    cleaned data and created object declaration

--- textBasedGame.py
+++ textBasedGame.py
@@ -5,32 +5,20 @@
 
 import os
 
-# Clear the screen based on the OS
-def clear_screen():
-    if os.name == 'posix':
-        os.system('clear')
-    else:
-        os.system('cls')
-
-# A dictionary for the simplified dragon text game
-# The dictionary links a room to other rooms.
-rooms = {
-    'Great Hall': {'South': 'Bedroom'},
-    'Bedroom': {'North': 'Great Hall', 'East': 'Cellar'},
-    'Cellar': {'West': 'Bedroom'}
+# map of rooms 
+roomsDirections = {
+    "Plane Wreck": {"E": "Crash Site"},
+    "Crash Site": {"E": "Jungle Path", "W": "Plane Wreck"},
+    "Jungle Path": {"N": "Top of Temple Stairs", "W" : "Crash Site"},
+    "Top of Temple Stairs": {"N" : "Temple Entrance", "S" : "Jungle Path"},
+    "Temple Entrance": {"N" : "Treasure Room", "S" : "Top of Temple Stairs", "E" : "Demon's Lair", "W" : "Creepy Library"},
+    "Creepy Library": {"N" : "Tomb", "E" : "Temple Entrance"},
+    "Tomb": {"S" : "Creepy Library", "E" : "Treasure Room"},
+    "Treasure Room": {"S" : "Temple Entrance", "W" : "Tomb"},
+    "Demon's Lair": {"W" : "Temple Entrance"}
 }
 
-class Room:
-    def __init__(self, name, description, items, characters):
-        self.name = name
-        self.description = description
-        self.items = items
-        self.characters = characters
-
-    def enter(self):
-        print(self.description)
-        # additional logic here
-
+# old room map 
 rooms = {
     "Plane Wreck": {"E": "Crash Site"},
     "Crash Site": {"E": "Jungle Path", "W": "Plane Wreck"},
@@ -43,9 +31,60 @@ rooms = {
     "Demon's Lair": {"W" : "Temple Entrance"}
 }
 
-playing = True
-currentRoom = 'Great Hall'
+# room descriptions
+roomDescriptions = {
+    "Plane Wreck": " ",
+    "Crash Site": " ",
+    "Jungle Path": " ",
+    "Top of Temple Stairs": " ",
+    "Temple Entrance": " ",
+    "Creepy Library": " ",
+    "Tomb": " ",
+    "Treasure Room": " ",
+    "Demon's Lair": " "
+}
 
+# room items
+roomItems = {
+    "Plane Wreck": {},
+    "Crash Site": {},
+    "Jungle Path": {},
+    "Top of Temple Stairs": {},
+    "Temple Entrance": {},
+    "Creepy Library": {},
+    "Tomb": {},
+    "Treasure Room": {},
+    "Demon's Lair": {}
+}
+
+# Clear the screen based on the OS
+def clear_screen():
+    if os.name == 'posix':
+        os.system('clear')
+    else:
+        os.system('cls')
+
+# class that contains methods for room logic 
+class Room:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = roomsDirections[self.name]
+        self.description = roomDescriptions[self.name]
+        self.items = roomItems[self.name]
+
+    def enter(self):
+        print(self.description)
+        # additional logic here
+
+# room object declaration
+rooms = ["Plane Wreck", "Crash Site", "Jungle Path", "Top of Temple Stairs", "Temple Entrance", "Creepy Library", "Tomb", "Treasure Room", "Demon's Lair"]
+roomObjMap = {x : Room(x) for x in rooms}
+
+# init starting room and game state
+playing = True
+currentRoom = 'Plane Wreck'
+
+# game loop
 while playing:
     # Display current room
     print(f"You are in {currentRoom}.")
