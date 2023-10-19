317f817a3b880402e714263b6eeb62f53f504351
neotions <johnmsthilaire@gmail.com>
2023-10-18 21:27:31 -0400

    more restructuring and writing functions for game methods

--- textBasedGame.py
+++ textBasedGame.py
@@ -6,6 +6,7 @@
 import os
 import random
 
+############  all game data ##############
 # map of rooms 
 roomsDirections = {
     "Plane Wreck": {"E": "Crash Site"},
@@ -45,6 +46,18 @@ roomDescriptions = {
     "Demon's Lair" : "Placeholder room description"
 }
 
+itemDescriptions = {
+    "Plane Wreck" : "Placeholder room description",
+    "Crash Site" : "Placeholder room description",
+    "Jungle Path" : "Placeholder room description",
+    "Top of Temple Stairs" : "Placeholder room description",
+    "Temple Entrance" : "Placeholder room description",
+    "Creepy Library" : "Placeholder room description",
+    "Tomb": "Placeholder room description",
+    "Treasure Room" : "Placeholder room description",
+    "Demon's Lair" : "Placeholder room description"
+}
+
 # room items
 roomItems = {
     "Plane Wreck": {},
@@ -58,61 +71,16 @@ roomItems = {
     "Demon's Lair": {}
 }
 
-# Clear the screen based on the OS
+############  functions and classes ##############
+# clear the screen based on the OS
 def clear_screen():
     if os.name == 'posix':
         os.system('clear')
     else:
         os.system('cls')
 
-
-
-# class that contains methods for room logic 
-class Room:
-    def __init__(self, name):
-        self.name = name
-        self.directions = roomsDirections[self.name]
-        self.description = roomDescriptions[self.name]
-        self.items = roomItems[self.name]
-
-    def enter(self):
-        print(self.description)
-        # additional logic here
-    
-    # prints description of room
-    def examine(self):
-        print(self.discription)
-
-    # search room for items and user choice to pick up item
-    def search(self):
-        print(f"You search the room and find:")
-        for i, item in enumerate(self.items):
-            print(f"{i+1}. {item}")
-            
-        itemSelection = int(input("Enter the number of the item you would like to pick up"))
-        if itemSelection - 1 <= len(self.items):
-            inventory.append(self.items[itemSelection - 1])
-            self.items.pop(itemSelection - 1)
-        else:
-            print("Enter a number listed")
-        
-
-# room object declaration
-rooms = ["Plane Wreck", "Crash Site", "Jungle Path", "Top of Temple Stairs", "Temple Entrance", "Creepy Library", "Tomb", "Treasure Room", "Demon's Lair"]
-
-# dictionary with all room objects
-roomObjMap = {x : Room(x) for x in rooms}
-
-# init starting room game state, and inventory
-playing = True
-currentRoom = roomObjMap['Plane Wreck']
-inventory = []
-
-# game loop
-while playing:
-    # Display current room
-    print(f"You are in {currentRoom.name}.")
-    print(" ")
+# move between rooms
+def moveRoom():
     # table header
     print(f"{'Direction':<15}{'Room':<25}")
     print('-' * 40)
@@ -134,7 +102,101 @@ while playing:
     else:
         currentRoom = roomObjMap[currentRoom.directions[newDirection]]
 
-    # Clear the screen
+# manage inventory prompts
+def useInventory(inventory):
+    if len(inventory == 0):
+        print("You don't have any items yet!")
+    else:
+        # table header
+        print(f"{'Item':<15}{'Description':<25}")
+        print('-' * 40)
+
+    # List available directions and rooms
+    for item in inventory:
+        print(f"{item:<15}{itemDescriptions[item]:<25}")
+    print()
+
+# exits game
+def leave():
+    input('Are you sure? (y or n)')
+    while input not in ['y','n']:
+        print('Invalid input')
+        input('Are you sure you want to quit? (y or n)\n')
+    if input == 'y':
+        playing = False
+
+# class that contains methods for room logic 
+class Room:
+    def __init__(self, name):
+        self.name = name
+        self.directions = roomsDirections[self.name]
+        self.description = roomDescriptions[self.name]
+        self.items = roomItems[self.name]
+
+    def enter(self):
+        print(self.description)
+    
+    # prints description of room
+    def examine(self):
+        print(self.discription)
+
+    # search room for items and user choice to pick up item
+    def search(self):
+        print(f"You search the room and find:")
+        for i, item in enumerate(self.items):
+            print(f"{i+1}. {item}")
+            
+        itemSelection = int(input("Enter the number of the item you would like to pick up"))
+        if itemSelection - 1 <= len(self.items):
+            inventory.append(self.items[itemSelection - 1])
+            self.items.pop(itemSelection - 1)
+        else:
+            print("Enter a number listed")
+
+
+############  init  ##############
+# room object declaration
+rooms = ["Plane Wreck", "Crash Site", "Jungle Path", "Top of Temple Stairs", "Temple Entrance", "Creepy Library", "Tomb", "Treasure Room", "Demon's Lair"]
+
+# dictionary with all room objects
+roomObjMap = {x : Room(x) for x in rooms}
+
+# init starting room, game states, and inventory
+playing = True
+complete = False
+currentRoom = roomObjMap['Plane Wreck']
+inventory = []
+
+############  game  ##############
+while playing:
+    # Display current room
+    print(f"You are in {currentRoom.name}.")
+    print(" ")
+
+    # ask user what to do
+    print("1. Move to another room")
+    print("2. Check Inventory")
+    print("3. Search Room")
+    print("4. Exit Game")
+    command = input('What would you like to do? (enter a number)\n')
+
+    # check if input is valid
+    while command not in [1,2,3,4]:
+        print('invalid input... (pick a number 1-4)')
+        command = int(input('What would you like to do? (enter a number)\n'))
+    # command cases
+    if command == 1:
+        moveRoom(currentRoom)
+    elif command == 2:
+        useInventory(inventory)
+    elif command == 3:
+        currentRoom.search()
+    elif command == 4:
+        leave()
+    else:
+        print("Not too sure how you got here.... that's a bug")
+
+    # clear the screen
     clear_screen()
 
 print("Thanks for Playing!!")
